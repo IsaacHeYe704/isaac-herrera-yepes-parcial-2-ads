fb23534798fe9bb8efb4f70c1462230a6aa3fa3c
Isaac Herrera <isaacheye@unisabana.edu.co>
2021-09-26 09:38:34 -0500

    finishing the readme

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ class Game {
   cl: any;
   rl: any;
   tipoDeJugador: Number;
-  dañoEnemiGo: number=15;
+  dañoEnemiGo: number = 15;
   menu: string = `Te debes enfrentar a un moustruo con \x1b[31m ${this.dañoEnemiGo} \x1b[0m de ataque, cual es tu decicion?: \n
   1)entrenar( es probable recuperes vida y que ganes talento para comprar equipamento)
   2)tienda(comprar equipamento, se usa talento)
@@ -31,7 +31,6 @@ class Game {
     });
     this.startMenu = new SelectMenus(this.rl);
     this.run();
-    
   }
   async run() {
     console.clear();
@@ -46,7 +45,6 @@ class Game {
     //tipo de jugador 2 crea un caballero
     //tipo de jugador 3 crea un arquero
     const jugadorFactory: JugadorConcreteFactory = new JugadorConcreteFactory();
-
     this.elJugador = jugadorFactory.crearJugador(tipoDeJugador, nombreJugador);
 
     this.play();
@@ -69,30 +67,35 @@ class Game {
       case 2: {
         //implementar tienda
         this.tienda = new Tienda(this.rl);
-        await this.tienda.mostrarTienda(this.elJugador).then((jugador)=>{
+        await this.tienda.mostrarTienda(this.elJugador).then((jugador) => {
           this.elJugador = jugador;
         });
-        
+
         break;
       }
       case 3: {
         this.elJugador.recibirDaño(this.dañoEnemiGo);
-        console.log(`this.dañoEnemiGo-this.elJugador.getVida()`, this.dañoEnemiGo-this.elJugador.getVida())
-        if(this.dañoEnemiGo-this.elJugador.getVida() <20)
-        {
+        console.log(
+          `this.dañoEnemiGo-this.elJugador.getVida()`,
+          this.dañoEnemiGo - this.elJugador.getVida()
+        );
+        if (this.dañoEnemiGo - this.elJugador.getVida() < 20) {
           console.log("\x1b[31m", `ganaste!!!!!!`);
           this.tomarDecision(5);
-        }else
-        {
-          console.log("\x1b[31m","oh no el moustruo ha regresado mas fuerte! ","\x1b[0m");
+        } else {
+          console.log(
+            "\x1b[31m",
+            "oh no el moustruo ha regresado mas fuerte! ",
+            "\x1b[0m"
+          );
           this.dañoEnemiGo += 20;
           this.menu = `Te debes enfrentar a un moustruo con \x1b[31m ${this.dañoEnemiGo} \x1b[0m de ataque, cual es tu decicion?: \n
             1)entrenar( es probable recuperes vida y que ganes talento para comprar equipamento)
             2)tienda(comprar equipamento, se usa talento)
             3)atacar
-            4)rendirte :( \n`
+            4)rendirte :( \n`;
         }
-        
+
         break;
       }
       case 4: {
@@ -105,24 +108,22 @@ class Game {
           process.exit();
         }, 1500);
         break;
-      
       }
-      case 5:{
-        
+      case 5: {
         let n: NodeJS.Timeout;
-      console.clear();
-      console.log("\x1b[31m", `ganaste!!!!!!`);
-      console.log("\x1b[31m", `el juego ha terminado....`);
-      n = await setTimeout(() => {
         console.clear();
-        this.playing = false;
-        process.exit();
-      }, 1500);
-      break;
+        console.log("\x1b[31m", `ganaste!!!!!!`);
+        console.log("\x1b[31m", `el juego ha terminado....`);
+        n = await setTimeout(() => {
+          console.clear();
+          this.playing = false;
+          process.exit();
+        }, 1500);
+        break;
       }
       default: {
         console.clear();
-        console.log("\x1b[31m","esa opcion no es valida!!!");
+        console.log("\x1b[31m", "esa opcion no es valida!!!");
       }
     }
   }
